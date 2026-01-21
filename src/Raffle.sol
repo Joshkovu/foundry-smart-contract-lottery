31209d714850f5876b421ca5ce496fe6566652a1
Joshkovu <joashkuteesa223@gmail.com>
2026-01-21 09:28:48 +0300

    feat:Initialize functions for raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,26 @@
+// Layout of Contract:
+// license
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
 // SPDX-License-Identifier:MIT
 pragma solidity 0.8.19;
 
@@ -9,13 +32,21 @@ pragma solidity 0.8.19;
  */
 
 contract Raffle {
+    /** Errors */
+    error Raffle__NotEnoughETHSent();
+
     uint256 private immutable I_ENTRANCE_FEE;
 
     constructor(uint256 entranceFee) {
         I_ENTRANCE_FEE = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= I_ENTRANCE_FEE, "Not enough ETH sent!");
+        if (msg.value < I_ENTRANCE_FEE) {
+            revert Raffle__NotEnoughETHSent();
+        }
+    }
 
     function pickWinner() public {}
 
